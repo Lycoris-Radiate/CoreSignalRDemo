8a7bfb07c4e5253e0f6621ff9d2ef6910f015c8f
胡文兵 <275031126@qq.com>
2020-05-01 16:00:45 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/HubsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using WebApplication1.Hubs;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HubsController : ControllerBase
+    {
+        private readonly IHubContext<MessageHub> _hubContext;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="hubClients"></param>
+        public HubsController(IHubContext<MessageHub> hubClients)
+        {
+            _hubContext = hubClients;
+        }
+        /// <summary>
+        /// SignalR推送
+        /// </summary>
+        /// <param name="loginNo"></param>
+        [HttpGet]
+        [Route("pushMsg")]
+        public void PushMsg(string loginNo)
+        {
+            if (string.IsNullOrWhiteSpace(loginNo))
+            {
+                //给所有人推送消息
+                _hubContext.Clients.All.SendAsync("ReceiveMessage", "这是控制器发送的消息");
+            }
+            else
+            {
+                //给指定人推送
+                MessageHub.Connections.TryGetValue(loginNo, out string id);
+                _hubContext.Clients.Client(id).SendAsync("ReceiveMessage", "这是控制器发送的消息");
+            }
+        }
+    }
+}
---
+++ WebApplication1/Hubs/MessageHub.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class MessageHub : Hub
+    {
+        /// <summary>
+        /// 存放已连接信息
+        /// </summary>
+        public static Dictionary<string, string> Connections = new Dictionary<string, string>();
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="loginNo"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string loginNo, string message, string fromloginNo)
+        {
+            Connections.TryGetValue(loginNo, out string clientId);
+            //ReceiveMessage 客户端接受方法
+            await Clients.Client(clientId).SendAsync("ReceiveMessage", message, fromloginNo);
+        }
+        //
+        public async Task GetAllUser()
+        {
+            await Clients.All.SendAsync("GetAllUser",JsonConvert.SerializeObject(Connections));
+        }
+        /// <summary>
+        /// 客户端登录成功保存用户账号和客户端Id
+        /// </summary>
+        /// <param name="loginNo"></param>
+        public void SendLogin(string loginNo)
+        {
+            //判断用户有没有登陆过(没登陆过插入用户名和Id，登陆过修改用户名和Id)
+            if (!Connections.ContainsKey(loginNo))
+            {
+                Connections.Add(loginNo, Context.ConnectionId);
+            }
+            else
+            {
+                Connections[loginNo] = Context.ConnectionId;
+            }
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("Disconnected->ConnectionId:" + this.Context.ConnectionId);
+            //Connections = Connections.Where(s => s.Value != this.Context.ConnectionId).ToDictionary(s => s.Key, s => s.Value);
+             //GetAllUser();
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
